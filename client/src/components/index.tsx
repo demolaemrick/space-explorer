39082d3431864013e288ab3588a1f1e3eaeb1c29
Emrick19 <emricky17@gmail.com>
2022-01-28 14:19:35 +0100

    Rename component

--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,7 +2,7 @@ export { default as Button } from "./Button";
 export { default as Footer } from "./Footer";
 export { default as Header } from "./Header";
 export { default as LaunchDetail } from "./LaunchDetail";
-export { default as LaunchTile } from "./launch-tile";
+export { default as LaunchTile } from "./launchTile";
 export { default as Loading } from "./Loading";
 export { default as LoginForm } from "./LoginForm";
 export { default as MenuItem } from "./ManuItem";
